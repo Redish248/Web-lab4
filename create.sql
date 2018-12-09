971d0464cabce9815e7d91fc80b2bf8064f03a30
Olga <olgasoboleva99@gmail.com>
2018-12-09 19:03:57 +0300

    Point table

--- create.sql
+++ create.sql
@@ -9,5 +9,6 @@ CREATE TABLE PointLab (
   x REAL NOT NULL,
   y REAL NOT NULL,
   r REAL NOT NULL,
-  is_In_Area BOOLEAN NOT NULL
+  is_In_Area BOOLEAN NOT NULL,
+  session_id varchar(40) NOT NULL
 );
